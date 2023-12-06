5e89d30c5735532121fc3ac774e950e888cdae52
Saurav Khanal <hi.sauravkhanal@gmail.com>
2023-12-06 22:13:21 +0545

    initial commit - works

--- src/index.js
+++ src/index.js
@@ -1,17 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+const Item = ({ imageUrl, name, description }) => {
+  return (
+    <article className='item' >
+      <img className='displayPic' src={imageUrl} alt={name} />
+      <h3 className='name'>{name}</h3>
+      <p className='description'>{description}</p>
+    </article>
+  )
+}
+
+
+const details = require('./data.json')
+const dataArray = details.data
+
+
+const ItemList = () => {
+  return (
+    <section className='itemList'>
+      {
+        dataArray.map((item) => {
+          return <Item imageUrl={item.imageUrl} name={item.name} description={item.description} key={item.id}/>
+        })
+      }
+    </section>
+  )
+}
+
+
+root.render(
+  <ItemList />
+)
