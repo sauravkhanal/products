f42a66009dab6a1ffe77ea719eb94d6ebdf6ac79
Saurav Khanal <hi.sauravkhanal@gmail.com>
2023-12-07 23:22:07 +0545

    all card of same size, create image container of 200px square hover effect: scale and change number bg color

--- src/item.js
+++ src/item.js
@@ -1,11 +1,18 @@
 const Item = ({ imageUrl, name, description, index }) => {
     return (
         <article className='item' >
-            <img className='displayPic' src={imageUrl} alt={name} />
+            {/* <img className='displayPic' src={imageUrl} alt={name} /> */}
+            <ImageContainer imageUrl={imageUrl} name={name}></ImageContainer>
             <h3 className='name'>{name}</h3>
             <p className='description'>{description}</p>
             <p className="index">#{index + 1}</p>
         </article>
     )
+}
+
+const ImageContainer = ({imageUrl, name}) => {
+    return <div className="imageContainer">
+        <img className="displayPic" src={imageUrl} alt={name} />
+    </div>
 }
 export default Item;
