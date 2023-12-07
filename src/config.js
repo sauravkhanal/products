233fb8225cd30bd7eaa271e0d67276c5acb6d32c
Saurav Khanal <hi.sauravkhanal@gmail.com>
2023-12-07 20:41:23 +0545

    api endpoint, http -> https

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-const apiEndpoint = "http://saurav.paudelrohan.com.np/Watches/GetAllWatches?password=pralu"
+const apiEndpoint = "https://saurav.paudelrohan.com.np/Watches/GetAllWatches?password=pralu"
 
 export { apiEndpoint }
